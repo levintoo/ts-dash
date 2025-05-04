68d14dc27db423dea16fa0fd1cfac0cb7c5c39b3
levintoo <tooklevin@gmail.com>
2025-05-04 22:15:24 +0300

    feat(inertia): install inertia-react with ts and ssr

---
+++ resources/js/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import {Head} from "@inertiajs/react";
+
+export default function Dashboard() {
+    return (
+        <>
+            <Head title="Dashboard"/>
+            <div>Hello from react with inertia</div>
+        </>
+    )
+}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Welcome');
+});
+
+Route::get('/dashboard', function () {
+    return inertia('Dashboard');
 });
