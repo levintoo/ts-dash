269d0359eca3a4297bbbecbf4e0c5377b188906c
levintoo <tooklevin@gmail.com>
2025-05-04 22:44:24 +0300

    feat(ui): install shadcn ui

--- resources/js/pages/Dashboard.tsx
+++ resources/js/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import {Head} from "@inertiajs/react";
+import {Button} from "@/components/ui/button";
 
 export default function Dashboard() {
     return (
-        <>
+        <div className="container mx-auto">
             <Head title="Dashboard"/>
             <div>Hello from react with inertia</div>
-        </>
+            <Button>Click Me</Button>
+        </div>
     )
 }
